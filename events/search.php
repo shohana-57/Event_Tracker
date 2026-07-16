2ee84316dbdeae49578b499fae75f67c6cf56024
Mukta Rani Baishnob <muktabaishnob16@gmail.com>
2026-07-16 20:24:45 +0600

    EVT-27 Add search results table

--- events/search.php
+++ events/search.php
@@ -2,3 +2,13 @@
     <h1>Search Events</h1>
     <a href="create.php" class="btn btn-primary">+ New Event</a>
 </div>
+
+<form method="GET" action="search.php" class="search-bar">
+    <label for="search_date" class="sr-only">Filter by date</label>
+    <input type="date" id="search_date" name="date"
+           value="<?php echo htmlspecialchars($searchDate); ?>">
+    <button type="submit" class="btn btn-secondary">Search</button>
+    <?php if ($searchDate !== ''): ?>
+        <a href="search.php" class="btn-clear">Clear</a>
+    <?php endif; ?>
+</form>
